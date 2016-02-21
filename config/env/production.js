b1ae3eb0b25e12d98651c2a0d93d3ad328614232
sanatik <bosone@ya.ru>
2016-02-22 02:48:55 +0600

    Roles added to production config

--- config/env/production.js
+++ config/env/production.js
@@ -4,30 +4,26 @@
 module.exports = {
     db: 'mongodb://expotest:expotest@ds059155.mongolab.com:59155/expotest',
     privateKey: 'SECRETKEY',
-    roles: [{
-        anonymous: {
+    roles: [
+        {
+            name: 'anonymous',
             id: -1,
             permissions: ['/auth/login', '/auth/signup', '/api/createOrUpdate', '/api/expositions', '/exposition']
         },
-        visitor: {
-            id: 3,
+        {
+            name: 'visitor',
+            id: -1,
             permissions: ['/auth/login', '/auth/signup', '/api/createOrUpdate', '/api/expositions', '/exposition']
         },
-        exponent: {
-            id: 1,
+        {
+            name: 'exponent',
+            id: -1,
             permissions: ['/auth/login', '/auth/signup', '/api/createOrUpdate', '/api/expositions', '/exposition']
         },
-        organizer: {
-            id: 2,
-            permissions: ['/auth/login', '/auth/signup', '/api/createOrUpdate', '/api/expositions', '/exposition', '/exposition/create']
-        },
-        moderator: {
-            id: 3,
-            permissions: ['/auth/login', '/auth/signup', '/api/createOrUpdate', '/api/expositions', '/exposition']
-        },
-        admin: {
-            id: 4,
+        {
+            name: 'organizer',
+            id: -1,
             permissions: ['/auth/login', '/auth/signup', '/api/createOrUpdate', '/api/expositions', '/exposition']
         }
-    }]
+    ]
 };
