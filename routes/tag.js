d90f4449ff1ead2331416f778a63a6710a3f05f8
sanatik <bosone@ya.ru>
2016-04-05 21:46:37 +0600

    Hack for creating themes

--- routes/tag.js
+++ routes/tag.js
@@ -5,6 +5,6 @@ var express = require('express');
 var router = express.Router();
 var tagController = require('../app/tags/controllers/TagController');
 
-router.post('/',tagController.create);
+router.get('/create',tagController.create);
 router.get('/',tagController.getAll);
 module.exports = router;
